bf91bad740c448458fbe7507021563b5cb7c3ea1
Orca-Itgit config --global user.email whitefox@kakao.com <whitefox@kakao.com>
2022-08-23 10:01:01 +0900

    라우팅 적용 및 헤더 메뉴 작성

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,19 +1,23 @@
-/* Header 영역 */
+/* Header */
 
 import { useState } from 'react';
 import style from '../css/header.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Header = () => {
 
-    const [sideMenuStatus, setSideMenuStatus] = useState<Boolean>(false);
-    const [userMenuStatus, setUserMenuStatus] = useState<Boolean>(false);
+    const [sideMenuStatus, setSideMenuStatus] = useState<Boolean>(false); // Mobile Screen -> Side Menu enadble, disable
+    const [userMenuStatus, setUserMenuStatus] = useState<Boolean>(false); // User Menu enable, disable
+    let navigate = useNavigate();
 
     return(
         <header className={style.header}>
-            <FontAwesomeIcon className={style.menuBars} icon={faBars} onClick={() => {setSideMenuStatus(true)}}/>
+            <FontAwesomeIcon className={style.menuBars} icon={faBars} onClick={() => {
+                setSideMenuStatus(true);
+                setUserMenuStatus(false);
+            }}/>
             {
                 sideMenuStatus == true ? 
                 <nav className={style.sideMenu}>
@@ -21,12 +25,32 @@ const Header = () => {
                         <h4 className={style.sideMenuTitle}>Menu</h4>
                         <FontAwesomeIcon className={style.closeSideMenu} icon={faXmark} onClick={() => {setSideMenuStatus(false)}}/>
                     </div>
+                    <ul className={style.sideMenuList}>
+                        <li><Link to="" className={style.sideMenuItem}>Store</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>Mac</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>iPad</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>iPhone</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>Watch</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>Airpods</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>TV & Home</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>Only on Apple</Link></li>
+                        <li><Link to="" className={style.sideMenuItem}>Support</Link></li>
+                    </ul>
                 </nav>
                 :
                 null
             }
-            <img src="/src/asset/Apple_Logo.png" alt="Logo" className={style.logo} />
-            <FontAwesomeIcon className={style.userIcon} icon={faUser} />
+            <img src="/src/asset/Apple_Logo.png" alt="Logo" className={style.logo} onClick={() => {navigate('/')}}/>
+            <Link to="" className={style.menuItem}>Store</Link>
+            <Link to="" className={style.menuItem}>Mac</Link>
+            <Link to="" className={style.menuItem}>iPad</Link>
+            <Link to="" className={style.menuItem}>iPhone</Link>
+            <Link to="" className={style.menuItem}>Watch</Link>
+            <Link to="" className={style.menuItem}>Airpods</Link>
+            <Link to="" className={style.menuItem}>TV & Home</Link>
+            <Link to="" className={style.menuItem}>Only on Apple</Link>
+            <Link to="" className={style.menuItem}>Support</Link>
+            <FontAwesomeIcon className={style.userIcon} icon={faUser} onClick={() => {userMenuStatus == true ? setUserMenuStatus(false) : setUserMenuStatus(true)}}/>
             {
                 userMenuStatus == true ?
                 <nav className={userMenuStatus == true ? style.padeInUserMenu : style.padeOutUserMenu}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Main from './pages/main';
+import Routing from './Routing';
 import { Reset } from 'styled-reset';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Reset />
-    <Main />
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
   </React.StrictMode>
 )
